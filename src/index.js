b8c1301c64cb74fcc39741ec797edc4a60f6cbe1
zenrs <67385651+zenrs@users.noreply.github.com>
2020-11-02 21:54:24 -0500

    Removed duplicate current location function

--- src/index.js
+++ src/index.js
@@ -140,13 +140,6 @@ function handleSubmit(event) {
   searchCity(city);
 }
 
-function searchLocation(position) {
-  let apiKey = "70e6d6d84c4425597feba736a42e0acf";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(displayWeather);
-}
-
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchLocation);
